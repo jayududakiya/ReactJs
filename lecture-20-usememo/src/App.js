f5ef39120aefe377dfcbc1a18b22491f514a3e81
JD <137084651+jayududakiya@users.noreply.github.com>
2024-04-21 23:10:48 +0530

    Topic : UseMemo and Count Code Practice

---
+++ lecture-20-usememo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UMemo from './UseMemos/Umemo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UMemo/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
